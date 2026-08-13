f6c6d9b89ecba849a8c333a7445fed2e41403e77
AkshayaKumar <KumarA2@cintas.com>
2026-08-13 17:24:53 +0530

    refactor login feature

---
+++ troy-ats/src/main/java/com/troy/ats/dto/EmployeeDto.java
@@ -0,0 +1,28 @@
+package com.troy.ats.dto;
+
+import com.troy.ats.enums.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeDto {
+
+    private UUID id;
+    private String employeeCode;
+    private String fullName;
+    private String designation;
+    private String officialEmail;
+    private String phone;
+    private String whatsapp;
+    private String photoUrl;
+    private UserRole role;
+    private LocalDateTime lastLoginAt;
+
+}
