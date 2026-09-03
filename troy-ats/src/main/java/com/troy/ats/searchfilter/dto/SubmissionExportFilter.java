18101a3d0c14f09a18a93ada423a23f4025f8104
AkshayaKumar <KumarA2@cintas.com>
2026-09-04 00:12:53 +0530

    create role v28

--- troy-ats/src/main/java/com/troy/ats/searchfilter/dto/SubmissionExportFilter.java
+++ troy-ats/src/main/java/com/troy/ats/searchfilter/dto/SubmissionExportFilter.java
@@ -3,12 +3,14 @@ package com.troy.ats.searchfilter.dto;
 import lombok.Data;
 
 import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Data
 public class SubmissionExportFilter {
 
     private UUID statusId;
+    private List<UUID> statusIds;
     private UUID jobId;
     private UUID clientId;
     private OffsetDateTime createdFrom;
--- troy-ats/src/main/java/com/troy/ats/searchfilter/dto/SubmissionFilter.java
+++ troy-ats/src/main/java/com/troy/ats/searchfilter/dto/SubmissionFilter.java
@@ -1,12 +1,14 @@
 package com.troy.ats.searchfilter.dto;
 
 import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public record SubmissionFilter(
         String search,
         String pipelineStage,
         UUID statusId,
+        List<UUID> statusIds,
         String statusName,
         String subStatusName,
         UUID candidateId,
